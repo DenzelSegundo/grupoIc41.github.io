5488aa3a6ec893625aa036524f8405abed51bbf4
DenzelSegundo <87163676+DenzelSegundo@users.noreply.github.com>
2021-07-09 00:34:55 -0500

    Add files via upload

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-   apiKey: "AIzaSyDmovq7T_-Ym7iutIjJIN1ABI33usaMmJo",
-    authDomain: "autenfi41.firebaseapp.com",
-    projectId: "autenfi41",
-    storageBucket: "autenfi41.appspot.com",
-    messagingSenderId: "777550911881",
-    appId: "1:777550911881:web:205487691c96b111382958",
-    measurementId: "G-YHTXKL0597"
+apiKey: "AIzaSyAWkaQsDhM21Nyh5WvNfTKeT0MgiYHJzFA",
+    authDomain: "autentifi41.firebaseapp.com",
+    projectId: "autentifi41",
+    storageBucket: "autentifi41.appspot.com",
+    messagingSenderId: "186347812788",
+    appId: "1:186347812788:web:5af8ba34fa2736b52ee4e1",
+    measurementId: "G-QRX5DLP67C"
 });
